5d42f601a73d9eff7642bed2fcfa966a97a59bd3
souf139 <soufianboushaba12@gmail.com>
2023-11-17 09:41:29 +0100

    this commit for sum updates

--- scripts.sql
+++ scripts.sql
@@ -1,25 +1,6 @@
 CREATE DATABASE IF NOT EXISTS conectevent;
 use conectevent;
 
-
-CREATE table if not EXISTS billets (
-   id_billet int PRIMARY KEY AUTO_INCREMENT,
-   id_participant int ,
-   id_evenement int, 
-   id_retour int ,
-   TypeBillet varchar(255),
-   FOREIGN KEY (id_retour) REFERENCES retours(id_retour),
-   FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
-   FOREIGN KEY (id_participant) REFERENCES participants(id_participant)
-)ENGINE=InnoDB;
-
-CREATE table if not EXISTS evenements (
-   id_evenement int PRIMARY KEY AUTO_INCREMENT,
-   id_lieu int ,
-   nom varchar(255),
-   FOREIGN KEY (id_lieu) REFERENCES lieux(id_lieu)
-)ENGINE=InnoDB;
-
 CREATE table if not EXISTS lieux (
    id_lieu int PRIMARY KEY AUTO_INCREMENT,
    placenom varchar(255)
@@ -30,13 +11,7 @@ CREATE table if not EXISTS organisateurs (
    nom varchar(255)
 )ENGINE=InnoDB;
 
-CREATE table if not EXISTS organisé (
-    id_organisateur int, 
-    id_evenement int , 
-    PRIMARY KEY (id_evenement,id_organisateur),
-    FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
-    FOREIGN KEY (id_organisateur) REFERENCES organisateurs(id_organisateur) 
-)ENGINE=InnoDB;
+
 
 CREATE table if not EXISTS participants  (
    id_participant int PRIMARY KEY AUTO_INCREMENT,
@@ -51,16 +26,54 @@ CREATE table if not EXISTS retours (
    montant varchar(255)
 )ENGINE=InnoDB;
 
-CREATE table if not EXISTS sponsoriser (
-   id_sponsor int, 
-    id_evenement int , 
-    PRIMARY KEY (id_sponsor , id_evenement)
-    FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
-    FOREIGN KEY (id_sponsor) REFERENCES sponsoriser(id_sponsor) 
-)ENGINE=InnoDB;
-
 
 CREATE table if not EXISTS sponsors  (
    id_sponsor int PRIMARY KEY AUTO_INCREMENT,
    nom varchar(255)
 )ENGINE=InnoDB;
+
+
+CREATE table if not EXISTS evenements (
+   id_evenement int PRIMARY KEY AUTO_INCREMENT,
+   id_lieu int ,
+   nom varchar(255),
+   FOREIGN KEY (id_lieu) REFERENCES lieux(id_lieu)
+)ENGINE=InnoDB;
+
+
+CREATE table if not EXISTS billets (
+   id_billet int PRIMARY KEY AUTO_INCREMENT,
+   id_participant int ,
+   id_evenement int, 
+   id_retour int ,
+   TypeBillet varchar(255),
+   FOREIGN KEY (id_retour) REFERENCES retours(id_retour),
+   FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
+   FOREIGN KEY (id_participant) REFERENCES participants(id_participant)
+)ENGINE=InnoDB;
+
+
+
+
+CREATE table if not EXISTS organisé (
+    id_organisateur int, 
+    id_evenement int , 
+    PRIMARY KEY (id_evenement,id_organisateur),
+    FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
+    FOREIGN KEY (id_organisateur) REFERENCES organisateurs(id_organisateur) 
+)ENGINE=InnoDB;
+
+
+CREATE table if not EXISTS sponsoriser (
+   id_sponsor int, 
+    id_evenement int , 
+    PRIMARY KEY (id_sponsor , id_evenement),
+    FOREIGN KEY (id_evenement) REFERENCES evenements(id_evenement),
+    FOREIGN KEY (id_sponsor) REFERENCES sponsors(id_sponsor) 
+)ENGINE=InnoDB;
+
+
+
+
+
+
